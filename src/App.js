46462194224b15195b7fd68a1448afeef395725b
Rodrigo Tiscareno <rodrigotiscareno1@gmail.com>
2023-03-01 11:00:19 -0500

    Lab 8

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import RestaurantScreen from './components/RestaurantScreen';
+import Counter from './features/counter/Counter';
 export default function App() {
   return (
     <div>
-      <RestaurantScreen />
+      <Counter></Counter>
     </div>
   );
 }
